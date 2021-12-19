af31382102dc750bc4b7a118fa74fb76f40219eb
sushanth510 <sushanthnani510@gmail.com>
2021-12-19 17:38:49 +0530

    resolved the bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,15 +26,15 @@ export class AppComponent {
   getHammingCode(){
     this.canCheck=true;
     this.hamCode="";
-    this.hammingCode[3]=this.dataBits[0];
-    this.hammingCode[5]=this.dataBits[1];
-    this.hammingCode[6]=this.dataBits[2];
-    this.hammingCode[7]=this.dataBits[3];
-    this.hammingCode[1]=((+(this.hammingCode[3]))^(+(this.hammingCode[5]))^(+(this.hammingCode[7]))).toString();
-    this.hammingCode[2]=((+(this.hammingCode[3]))^(+(this.hammingCode[6]))^(+(this.hammingCode[7]))).toString();
-    this.hammingCode[4]=((+(this.hammingCode[5]))^(+(this.hammingCode[6]))^(+(this.hammingCode[7]))).toString();
+    this.hammingCode[1]=this.dataBits[0];
+    this.hammingCode[2]=this.dataBits[1];
+    this.hammingCode[3]=this.dataBits[2];
+    this.hammingCode[5]=this.dataBits[3];
+    this.hammingCode[7]=((+(this.hammingCode[5]))^(+(this.hammingCode[3]))^(+(this.hammingCode[1]))).toString();
+    this.hammingCode[6]=((+(this.hammingCode[5]))^(+(this.hammingCode[2]))^(+(this.hammingCode[1]))).toString();
+    this.hammingCode[4]=((+(this.hammingCode[3]))^(+(this.hammingCode[2]))^(+(this.hammingCode[1]))).toString();
     console.log(this.hammingCode)
-    for(let i=7;i>=1;i--){
+    for(let i=1;i<=7;i++){
       this.hamCode=this.hamCode+this.hammingCode[i];
     }
   }
@@ -42,31 +42,36 @@ export class AppComponent {
   check(){
     this.correctedHamCode="";
     this.position=0;
-    
-    for(let i=1;i<=7;i++){
-      this.errorhammingCode[i]=this.checkBits[i-1];
-    }
-    console.log(this.errorhammingCode);
-    this.c1=(+(this.errorhammingCode[1]))^(+(this.errorhammingCode[3]))^(+(this.errorhammingCode[5]))^(+(this.errorhammingCode[7]));
-    this.c2=(+(this.errorhammingCode[2]))^(+(this.errorhammingCode[3]))^(+(this.errorhammingCode[6]))^(+(this.errorhammingCode[7]));
-    this.c3=(+(this.errorhammingCode[4]))^(+(this.errorhammingCode[5]))^(+(this.errorhammingCode[6]))^(+(this.errorhammingCode[7]));
-    this.position=this.c1*1+this.c2*2+this.c3*4;
-    console.log(this.position,this.c1,this.c2,this.c3);
-    if(this.position==0){
-      this.checkMessage="No error detected";
+    if(this.checkBits.length!=7){
+      this.checkMessage="Enter 7-bit Hamming Code";
     }
     else{
-      this.checkMessage="Error detected at position "+(this.position).toString();
+      for(let i=1;i<=7;i++){
+        this.errorhammingCode[i]=this.checkBits[i-1];
+      }
+      console.log(this.errorhammingCode);
+      this.c1=(+(this.errorhammingCode[7]))^(+(this.errorhammingCode[5]))^(+(this.errorhammingCode[3]))^(+(this.errorhammingCode[1]));
+      this.c2=(+(this.errorhammingCode[6]))^(+(this.errorhammingCode[5]))^(+(this.errorhammingCode[2]))^(+(this.errorhammingCode[1]));
+      this.c3=(+(this.errorhammingCode[4]))^(+(this.errorhammingCode[3]))^(+(this.errorhammingCode[2]))^(+(this.errorhammingCode[1]));
+      this.position=this.c1*1+this.c2*2+this.c3*4;
+      console.log(this.position,this.c1,this.c2,this.c3);
+      if(this.position==0){
+        this.checkMessage="No error detected";
+      }
+      else{
+        this.checkMessage="Error detected at position "+(8-this.position).toString();
+      }
     }
+    
   }
 
   correct(){
     
-    if(this.errorhammingCode[this.position]=='0'){
-      this.errorhammingCode[this.position]='1';
+    if(this.errorhammingCode[8-this.position]=='0'){
+      this.errorhammingCode[8-this.position]='1';
     }
     else{
-      this.errorhammingCode[this.position]='0';
+      this.errorhammingCode[8-this.position]='0';
     }
     console.log(this.errorhammingCode);
     for(let i=1;i<=7;i++){
